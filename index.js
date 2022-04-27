208c35ae7954c1a5c3854026fb88ca6aedb783e5
Norah Taylor <emt368@gmail.com>
2022-04-27 12:57:02 -0400

    commit tree 8bc3220fe9c601211240a87e8dc3379687ba6f71

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+//const newArray = [...array]
+
+const array = []
+//const newCards = [...array, writeCards]
+
+
+function writeCards(name, event){
+    for (let i = 0 ; i < name.length; i++){
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}   
+    //const newArray = [...array]
+    //return newArray
+
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown() {
+let countDown = 10;
+while (countDown >= 0) {
+    console.log(countDown--);
+} }
+countDown(9)
+
+// function countDown(number){
+//     for (let i = 10 ; i < number.length; i++){
+//         console.log(number);
+//     }
+//     return number
+// }
+// countDown(9)
